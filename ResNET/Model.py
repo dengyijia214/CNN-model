2e382ae49762abf65863a4fa6eefe3774899eea8
dengyijia214 <84968887+dengyijia214@users.noreply.github.com>
2022-06-11 23:45:34 +0800

    Update Model.py

--- ResNET/Model.py
+++ ResNET/Model.py
@@ -44,4 +44,40 @@ class Bottleneck(nn.Module):
     self.cn3 = nn.Conv2d(in_channels=out_channel, out_channels=out_channel*self.expansion, kernel_size=1, stride=1, bias=False)
     self.bn3 = nn.BatchNorm2d(out_channel*self.expansion)
     self.relu = nn.ReLU(inplace=True)
+    self.downsample = downsample
+    
+  def forward(self, x):
+    identity = x
+    if self.downsample is not None:
+      identity = self.downsample(x)
+      
+    out = self.conv1(x)
+    out = self.bn1(out)
+    out = self.relu(out)
+    
+    out = self.conv2(out)
+    out = self.bn2(out)
+    out = self.relu(out)
+    
+    out = self.conv3(out)
+    out = self.bn3(out)
+    
+    return out
+  
+class ResNet(nn.Module):
+  
+  def __init__(self, block, block_num, num_classes = 1000, include_top = True):
+    #这里的block是上面我们定义的残差结构，18和34用的是basicblock, 50等用的就是bottlenet
+    #block_num指定的是我们用的残差模块的个数
+    #这里的一千是因为Resnet在这里参加的是imagenet，这里是一个一千分类的问题，可以改成你的分类问题的种类
+    #include_top是问了方便在以后对残差网络进行升级设置的
+    super(ResNet, self).__init__()
+    self.include_top = include_top
+    self.in_channel = 64
+    
+    self.conv1 = nn.Conv2d(3, self.in_channel, kernel_size=7, stride=2, padding=3, bias=False)
+    self.bn1 = nn.BatchNorm2d(self.in_channel)
+    self.relu = nn.ReLU(inplace=True)
+    self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+    self.layer1 = self.
     
